3ecc27a346e43a922927a6f91ade398ded1b90f4
András Kalapos <43972335+kaland313@users.noreply.github.com>
2023-02-02 11:09:11 +0000

    Update requirements to support newer pytorch-lightning

--- setup.py
+++ setup.py
@@ -17,10 +17,16 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+import os
+
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
 KW = ["artificial intelligence", "deep learning", "unsupervised learning", "contrastive learning"]
 
+REQUIREMENTS_FILE = os.path.join(os.path.dirname(__file__), "requirements.txt")
+with open(REQUIREMENTS_FILE) as fo:
+    REQUIREMENTS = [str(req) for req in parse_requirements(fo.readlines())]
 
 EXTRA_REQUIREMENTS = {
     "dali": ["nvidia-dali-cuda110"],
@@ -43,18 +49,7 @@ setup(
     author_email="vturrisi@gmail.com, enrico.fini@gmail.com",
     url="https://github.com/vturrisi/solo-learn",
     keywords=KW,
-    install_requires=[
-        "torch==1.10.0",
-        "torchvision",
-        "einops",
-        "pytorch-lightning==1.5.3",
-        "torchmetrics==0.6.0",
-        "lightning-bolts>=0.4.0",
-        "tqdm",
-        "wandb",
-        "scipy",
-        "timm",
-    ],
+    install_requires=REQUIREMENTS,
     extras_require=EXTRA_REQUIREMENTS,
     dependency_links=["https://developer.download.nvidia.com/compute/redist"],
     classifiers=[
